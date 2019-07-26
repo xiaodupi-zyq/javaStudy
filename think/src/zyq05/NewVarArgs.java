6751560a065d539d0fbe4a2e0c836d592848fd8a
Zhuyuanqing_zyq <741883796@qq.com>
2019-07-26 10:12:42 +0800

    frist

---
+++ think/src/zyq05/NewVarArgs.java
@@ -0,0 +1,16 @@
+package zyq05;
+
+public class NewVarArgs {
+    static void printArray(Object...args) {//...代表可变参数
+        for (Object obj : args) {
+            System.out.println(obj + " ");
+        }
+        System.out.println();
+    }
+    public static  void main(String[] args) {
+        printArray(new Integer(47),new Float(3.14),new Double(11.11));
+        printArray();
+
+    }
+
+}
---
+++ think/src/zyq05/Test17.java
@@ -0,0 +1,20 @@
+package zyq05;
+
+class Dog{
+    Dog(String info){
+        System.out.println(info);
+    }
+}
+
+
+public class Test17 {
+
+    public static void main(String[] args){
+        Dog[] dogs = new Dog[20];
+        for (int i=0;i < 20;i++){
+            dogs[i] = new Dog(String.valueOf(i));
+        }
+        System.out.println("down");
+    }
+
+}
