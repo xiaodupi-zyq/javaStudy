71547f50de3c24b508b22619e1e8373ec86481df
Zhuyuanqing_zyq <741883796@qq.com>
2019-07-30 09:36:05 +0800

    7.4

--- think/src/zyq07/Detergent.java
+++ think/src/zyq07/Detergent.java
@@ -3,6 +3,7 @@ package zyq07;
 /**
  * 实现继承的相关方法，————————继承
  */
+/*
 class Cleanser {
     private String s = "Cleanser";
     public void append(String a) { s+=a; }
@@ -57,4 +58,46 @@ class SuperDetergent extends Detergent{
         superDetergent.sterilize();
         System.out.println(superDetergent);
     }
+}
+*/
+class Cleanser {
+    private String s = "Cleanser";
+    public void append(String a) { s+=a; }
+    public void dilute() {append(" dilute()");}
+    public void apply() {append(" apply()");}
+    public void scrub() {append(" scrub()");}
+    public String toString() {return s;}
+    public static void main(String[] args) {
+        Cleanser x = new Cleanser();
+        x.dilute();x.apply();x.scrub();
+        System.out.println(x);
+    }
+}
+
+public class Detergent extends Cleanser {
+    private Cleanser cleanser = new Cleanser();
+    private String s = "Detergent";
+    public void append(String a) {
+        cleanser.append(a);
+    }
+    public void dilute(){
+        cleanser.dilute();
+    }
+    public void apply(){
+        cleanser.apply();
+    }
+    public void scrub(){
+        cleanser.scrub();
+    }
+    public String toString(){
+        return cleanser.toString();
+    }
+    public static void main(String[] args){
+        Detergent detergent = new Detergent();
+        detergent.dilute();
+        detergent.apply();;
+        detergent.scrub();
+        System.out.println(detergent);
+    }
+
 }
---
+++ think/src/zyq07/SpaceShipDelegation.java
@@ -0,0 +1,32 @@
+package zyq07;
+
+public class SpaceShipDelegation {
+    private String name;
+    private SpaceShipControls controls = new SpaceShipControls();
+    public SpaceShipDelegation(String name) {
+        this.name = name;
+    }
+
+    public void back(int velocity) {
+        controls.back(velocity);
+    }
+    public void up(int velocity) {
+        controls.up(velocity);
+    }
+    public void down(int velocity) {
+        controls.down(velocity);
+    }
+    public void forward(int velocity) {
+        controls.forward(velocity);
+    }
+    public void left(int velocity) {
+        controls.left(velocity);
+    }
+    public void right(int velocity) {
+        controls.right(velocity);
+    }
+    public static void main(String[] args){
+        SpaceShipDelegation protector = new SpaceShipDelegation("NASA protector");
+        protector.forward(1000);
+    }
+}
