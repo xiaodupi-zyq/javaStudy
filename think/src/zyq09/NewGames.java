d8a1d8fef35c9209cc6ac480436b95f260a4e552
Zhuyuanqing_zyq <741883796@qq.com>
2019-08-09 11:29:53 +0800

    第九章 接口

---
+++ think/src/zyq09/NewGames.java
@@ -0,0 +1,45 @@
+package zyq09;
+
+import java.util.Random;
+
+interface NewGame {
+    void play();
+}
+
+interface NewGameFactory{
+    NewGame getNewGame();
+}
+
+class Coin implements NewGame{
+    public void play(){
+        Random random = new Random();
+        System.out.println("Coin this times is " + random.nextInt(2));
+    }
+}
+class CoinFactory implements NewGameFactory{
+    public NewGame getNewGame(){
+        return new Coin();
+    }
+}
+
+class Dice implements NewGame{
+    public void play(){
+        Random random = new Random();
+        System.out.println("Dice this times is " + (random.nextInt(6)+1));
+    }
+}
+class DiceFactory implements NewGameFactory{
+    public NewGame getNewGame(){
+        return new Dice();
+    }
+}
+
+public class NewGames {
+    public static void play(NewGameFactory newGameFactory){
+        newGameFactory.getNewGame().play();
+    }
+    public static void main(String[] args){
+        play(new CoinFactory());
+        play(new DiceFactory());
+    }
+}
