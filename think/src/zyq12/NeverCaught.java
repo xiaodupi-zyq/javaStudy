22847879985807a6b131fb7b6d1191289d701706
Zhuyuanqing_zyq <741883796@qq.com>
2019-09-08 17:29:48 +0800

    12.7

---
+++ think/src/zyq12/NeverCaught.java
@@ -0,0 +1,13 @@
+package zyq12;
+
+public class NeverCaught {
+    static void f(){
+        throw new RuntimeException("From f()");
+    }
+    static void g(){
+        f();
+    }
+    public static void main(String[] args){
+        g();
+    }
+}
