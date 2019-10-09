3ac0d4c2db3324fc99cfb0a2d1a624c72b582c6d
Zhuyuanqing_zyq <741883796@qq.com>
2019-10-09 09:00:39 +0800

    15.9 边界

---
+++ think/src/zyq15/GenericsAndCovariance.java
@@ -0,0 +1,17 @@
+package zyq15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Fruit{}
+class Apple extends Fruit{}
+class Jonathan extends Apple{}
+class Orange extends Fruit{}
+
+public class GenericsAndCovariance {
+    public static void main(String[] args){
+        List<? extends Fruit> flist = new ArrayList<Apple>();
+        flist.add(null);
+        Fruit f = flist.get(0);
+    }
+}
