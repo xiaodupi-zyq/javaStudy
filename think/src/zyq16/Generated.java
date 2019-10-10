d5bd6195471651dc69931e35ccf81e362387b94a
Zhuyuanqing_zyq <741883796@qq.com>
2019-10-10 09:32:25 +0800

    第十六章 数组，数组的相关操作，创建，使用，填充，判断相等，排序，二分查找，六个函数，equals、toString、hashcode、sort、fill、binarysearch

---
+++ think/src/zyq16/Generated.java
@@ -0,0 +1,38 @@
+package zyq16;
+
+import zyq15.Generator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Generated {
+    public static <T> T[] array(T[] a, Generator<T> gen){
+        return new CollectionData<T>(gen,a.length).toArray(a);
+    }
+    @SuppressWarnings("unchecked")
+    public static <T> T[] array(Class<T> type, Generator<T> gen, int size){
+        T[] a = (T[]) java.lang.reflect.Array.newInstance(type,size);
+        return new CollectionData<T>(gen,size).toArray(a);
+    }
+}
+class CollectionData<T> extends ArrayList<T>{
+    public CollectionData(Generator<T> gen, int quantity){
+        for(int i = 0; i < quantity; i++){
+            add(gen.next());
+        }
+    }
+//    public static <T> CollectionData<T> list(Generator<T> gen, int quantity) {
+//        return new CollectionData<>(gen, quantity);
+//    }
+}
+
+class TestGenerated {
+    public static void main(String[] args){
+        Integer[] a = {9,8,7,6};
+        System.out.println(Arrays.toString(a));
+        a = Generated.array(a,new CountingGenerator.Integer());
+        System.out.println(Arrays.toString(a));
+        Integer[] b = Generated.array(Integer.class,new CountingGenerator.Integer(),15);
+        System.out.println(Arrays.toString(b));
+    }
+}
