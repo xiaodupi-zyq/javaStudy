166f3b8736587fc6f16af6d8eef1fa97f12f1a85
Zhuyuanqing_zyq <741883796@qq.com>
2019-10-17 22:18:35 +0800

    第十七章，容器深入理解，hashcode，不同容器不同的底层原理，相关的性能比较，以及实用的方法

---
+++ think/src/zyq17/Test.java
@@ -0,0 +1,33 @@
+package zyq17;
+
+public abstract class Test<C> {
+    String name;
+    public Test(String name) {
+        this.name = name;
+    }
+    abstract int test(C container,TestParam tp);
+}
+class TestParam {
+    public final int size;
+    public final int loops;
+    public TestParam(int size,int loops){
+        this.size = size;
+        this.loops = loops;
+    }
+    public static TestParam[] array(int ... values){
+        int size = values.length/2;
+        TestParam[] result = new TestParam[size];
+        int n = 0;
+        for(int i = 0; i < size; i++){
+            result[i] = new TestParam(values[n++],values[n++]);
+        }
+        return result;
+    }
+    public TestParam[] array(String[] values){
+        int[] vals = new int[values.length];
+        for(int i = 0; i < vals.length; i++){
+            vals[i] = Integer.decode(values[i]);
+        }
+        return array(vals);
+    }
+}
