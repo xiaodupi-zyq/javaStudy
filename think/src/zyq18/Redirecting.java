4374a808414c2a46f376ddd57cc335373cea3159
Zhuyuanqing_zyq <741883796@qq.com>
2019-10-23 09:50:13 +0800

    第十八章，JAVA的IO ，主要讲述了java文件的输入输出，标准IO，对象的序列化，持久性，序列化的特性控制，主要还是理解序列化及其持久性控制，以及文件的输入输出流的相关问题，ByteBuffer的不同窗口对底层的影响

---
+++ think/src/zyq18/Redirecting.java
@@ -0,0 +1,21 @@
+package zyq18;
+
+import java.io.*;
+
+public class Redirecting {
+    public static void main(String[] args) throws IOException{
+        PrintStream console = System.out;
+        BufferedInputStream in = new BufferedInputStream(new FileInputStream("E:\\java学习\\think\\think\\src\\zyq18\\Redirecting.java"));
+        PrintStream out = new PrintStream(new BufferedOutputStream(new FileOutputStream("test.out")));
+        System.setIn(in);
+        System.setOut(out);
+        System.setErr(out);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while((s = br.readLine()) != null){
+            System.out.println(s);
+        }
+        out.close();
+        System.setOut(console);
+    }
+}
