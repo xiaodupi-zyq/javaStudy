c46106353d6f3d08c71e7ebb0c1c4f6be0a3f84e
Zhuyuanqing_zyq <741883796@qq.com>
2019-11-01 17:31:45 +0800

    20.4

---
+++ think/src/zyq20/database/TableCreator.java
@@ -0,0 +1,9 @@
+package zyq20.database;
+
+public class TableCreator {
+    public static void main(String[] args) throws Exception{
+        if(args.length < 1){
+            System.out.println("arguments: annotated classes");
+        }
+    }
+}
